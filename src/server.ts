a6234d2ae4501150a55eb4d1283dc4d008ef6b58
mehadishisir <mehadishisir@gmail.com>
2026-05-16 13:15:42 +0600

    understand express request and response

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,16 @@
-import express from "express"
-const app = express()
+import express, { type Application, type Request, type Response } from "express"
+const app :Application = express()
 const port = 5000
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (req:Request, res:Response) => {
+//   res.send('Hello World!')
+res.status(200).json({
+    "message":"welcome"
+})
+})
+app.post ('/',async(req:Request,res:Response)=>{
+  console.log(req.body)
 })
 
 app.listen(port, () => {
